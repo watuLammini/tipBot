bbc85360005d5ad40e61d9424b99d0838ba6e3ca
Ruben Lenz <ruben.lenz@campus.lmu.de>
2020-08-29 16:16:16 +0200

    Add some formulas for mean and standard deviation and probabilities

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,8 +1,12 @@
 module Lib
-    ( someFunc,
+    {-( someFunc,
       getNormDistrProb,
-      getCategorialProb
-    ) where
+      getCategorialProb,
+      getMean
+    )-} where
+
+import Types
+import qualified Data.Map as Map
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -13,3 +17,27 @@ getCategorialProb numberOfUnion totalNumber = numberOfUnion / totalNumber
 getNormDistrProb :: Double -> Double -> Double -> Double
 getNormDistrProb x mean sd = exp (-((x-mean)^2 / (2*sd*sd))) / sqrt (2*sd*sd*pi)
 
+getMean :: [Double] -> Double
+getMean xs = let sum = foldr (+) 0 xs
+                 len = length xs
+             in sum / (fromIntegral len)
+
+--getStandardDeviation :: [Double] -> Double -> Double
+--getStandardDeviation xs mean = undefined
+
+stan xs mean = sqrt (totalSum / (fromIntegral $ length xs))
+       where innerSum = map (flip (-) mean) xs
+             quadrSum = map (^2) innerSum
+             totalSum = foldr (+) 0 $ quadrSum
+
+allPoints2019 = map fromIntegral $ map points2019 $ Map.elems testMap :: [Double]
+allPoints2018 = map fromIntegral $ map points2018 $ Map.elems testMap :: [Double]
+mean2019 = getMean allPoints2019
+mean2018 = getMean allPoints2018
+stan2019 = stan allPoints2019 mean2019
+stan2018 = stan allPoints2018 mean2018
+
+-- Vorläufige Berechnung, es fehlt aber noch die bedingte Wahrscheinlichkeit
+-- dass Mannschaft Punkte x hat gegeben Spielergebnis y
+prob = (getNormDistrProb 82 mean2019 stan2019) * (getNormDistrProb 78 mean2018 stan2018) * 0.0879
+-- TODO: Fehlerbehandlung!:
--- src/Types.hs
+++ src/Types.hs
@@ -1,21 +1,39 @@
-module Types
-  ( Team
-  ) where
+module Types where
 
 import qualified Data.Map as Map
+import qualified Data.Vector as V
+import qualified Statistics.Distribution.Normal as Nor
 
 data Team = Team {
   name :: String,
-  points :: Int
-  }
+  points2019 :: Int,
+  points2018 :: Int
+  } deriving Show
 
 -- Test data
 
 fcb = Team {
-  name="FC Bayern",
-  points=82
+  name = "FC Bayern",
+  points2019 = 82,
+  points2018 = 78
 }
 
-testMap = Map.insert (name fcb) fcb Map.empty
+bdortm = Team {
+  name = "Borussia Dortmund",
+  points2019 = 69,
+  points2018 = 76
+}
+
+testMap = Map.insert (name bdortm) bdortm .
+          Map.insert (name fcb) fcb $ Map.empty
+
+testResults = Map.insert "1:1" 0.1173 .
+              Map.insert "2:1" 0.0879 .
+              Map.insert "1:0" 0.0813 .
+              Map.insert "2:0" 0.0794 $ Map.empty
 
 lookMeUp = case (Map.lookup "FC Bayern" testMap) of Just team -> name team
+
+-- Haut nicht hin :'(
+-- points2019V = V.fromList $ (Prelude.map fromIntegral (Map.elems $ Map.map points2019 testMap) :: [Double])
+-- normDistr = Distr.fromSample points2019V
